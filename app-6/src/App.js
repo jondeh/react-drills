d2384e9dca8a6a063e7fe06f85ba6f5d987d607d
jondeh <dehlinjd@gmail.com>
2020-02-26 20:26:36 -0700

    sixth commit

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ToDo from './Components/Todo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+      super()
+        this.state = {
+          list: [],
+          text: ''
+        }
+        this.updateList = this.updateList.bind(this)
+  }
+  
+  updateList(){
+    this.setState({
+      list: [...this.state.list, this.state.text],
+      input: ''
+    });
+  }
+
+  render(){
+    let list = this.state.list.map((e, i) => { 
+    return <ToDo key={i} task={e} />; 
+  });
+    
+    return (
+      <div className="App">
+        <h1>TO DO LIST</h1>
+        
+        <div>
+        <input
+          value={this.state.text} 
+          placeholder="Anotha one"
+          onChange={e => this
+          .setState({text: e.target.value})}/>
+          
+          <button onClick={this.updateList}>SUBMIT</button>
+        </div>
+            
+        <br />
+
+            {list}
+
+      </div>
+    );
+  }
 }
 
 export default App;
